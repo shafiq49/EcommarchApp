349d2fb3fa1313adfe1156e31764dab09a1ed443
shafiq49 <shafiqul.haque.milil@gmail.com>
2025-10-07 17:11:30 +0600

    static Crud operation

--- EcommarchApp/Program.cs
+++ EcommarchApp/Program.cs
@@ -23,30 +23,66 @@ app.UseAuthorization();
 List<Category> Categories = new List<Category>();
 
 //Get  categorite 
-app.MapGet("/api/category", () =>
+app.MapGet("/api/categories", () =>
 {
     return Results.Ok(Categories);
 });
 
-app.MapGet("/", () => "API testing perfectly");
+app.MapPost("/api/categories", () =>
+{
+    var newCategory = new Category
+    {
+       CategoryId = Guid.Parse("aa2c90bf-6ae7-4d78-a160-154ac35e9843"),
+       Name = "Electronics",
+       Description = "Electronics devices",
+       CreatedAt = DateTime.UtcNow,
+    };
+    Categories.Add(newCategory);
+    return Results.Created($"/api/categories/{newCategory.CategoryId}",newCategory);
+});
 
-app.MapGet("/hello",() =>
+app.MapDelete("/api/categories", () =>
 {
-    var resposne = new { message = "Get Successfull message", success = true };
-    return Results.Ok(resposne);
-});
-app.MapPost("/hello", () => 
-{
-    return Results.Created();
-});
-app.MapPut("/hello", () =>
-{
+    var foundCategory = Categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("aa2c90bf-6ae7-4d78-a160-154ac35e9843"));
+    if (foundCategory == null)
+    {
+        return Results.NotFound("Category Not Found");
+    }
+    Categories.Remove(foundCategory);
     return Results.NoContent();
 });
-app.MapDelete("/hello", () =>
+
+app.MapPut("/api/categories", () =>
 {
+    var foundCategory = Categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("aa2c90bf-6ae7-4d78-a160-154ac35e9843"));
+    if (foundCategory == null)
+    {
+        return Results.NotFound("Category Not Found");
+    }
+    foundCategory.Name = "Shafiq";
+    foundCategory.Description = "Shafiq is a name";   
     return Results.NoContent();
 });
+
+//app.MapGet("/", () => "API testing perfectly");
+
+//app.MapGet("/hello",() =>
+//{
+//    var resposne = new { message = "Get Successfull message", success = true };
+//    return Results.Ok(resposne);
+//});
+//app.MapPost("/hello", () => 
+//{
+//    return Results.Created();
+//});
+//app.MapPut("/hello", () =>
+//{
+//    return Results.NoContent();
+//});
+//app.MapDelete("/hello", () =>
+//{
+//    return Results.NoContent();
+//});
 
 app.MapControllers();
 
@@ -54,7 +90,7 @@ app.Run();
 
 public record Category
 {
-    public Guid ProductId { get; set; }
+    public Guid CategoryId { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public DateTime CreatedAt { get; set; }  
